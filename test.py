3ffbf80e26a65ec22ca47c690ab21475d0ac5f01
Gérald LEBAN <gerald.leban0@gmail.com>
2020-08-15 20:03:42 +0200

    passed

--- test.py
+++ test.py
@@ -1,37 +1,30 @@
-from DataClasses.Payload.Payload import *
-from DataClasses.Response.Response import *
-
+from Payload.Payload import Payload
 import requests
+import json
 
 # area = Area(lat=43.703284, lng=1.80605, radius=20000)
 # loc = Location(area=area, region=set_region("aquitaine"))
 
-payload = Payload(
-    limit=35,
-    limit_alu=0,
-    filters=Filters(
-        category=Category(id=get_category("google home")),
-        enums=Enums(ad_type=["offer"]),
-        location=Locale(department=set_dept("tarn")),
-        keywords=Keywords(text="google home"),
-        ranges=Ranges()
+
+def callAPI(payload):
+    return requests.post(
+        url="https://api.leboncoin.fr/finder/search",
+        data=payload,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0',
+            'Accept': '*/*',
+            'DNT': '1',
+        }
     )
-).to_json
 
-reponse = Response(**requests.post(
-    url="https://api.leboncoin.fr/finder/search",
-    data=payload,
-    headers={
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0',
-        'Accept': '*/*',
-        'DNT': '1',
-    }  # Fake User-Agent
-).json())
+# payload = {"limit":0,"limit_alu":0,"filters":{"category":{},"enums":{"ad_type":["offer"]},"location":{"locations":[{"locationType":"region","label":"Midi-Pyrénées","region_id":"16"}]},"keywords":{"text":"google home","parrot_used":4},"ranges":{"price":{"max":100}}}}
+# payload = json.dumps(payload)
+# print(callAPI(payload).json())
 
-
-for ads in reponse.ads:
-    offre = Ads(**ads)
-    print(f"Titre: {offre.subject}")
-    print(f"Prix: {offre.price[0]}€")
-    print(f"Ville: {Location(**offre.location).city_label}")
-    print("")
+pl = Payload()
+pl.setLimit(30)
+pl.maxPrice(200)
+pl.setDepartement("tarn")
+pl.searchFor("google home", True)
+print(pl.build())
+print(callAPI(pl.build()).json())
